7b0478d4a35d8f043b7da2fd2a4852203494779b
bambooharu <bambooharu0802@gmail.com>
2021-07-24 22:32:20 +0900

    SVG変更🌹

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var stroke;
 stroke = new Vivus('mask', {//アニメーションをするIDの指定
     start:'manual',//自動再生をせずスタートをマニュアルに
     type: 'scenario-sync',// アニメーションのタイプを設定
-    duration: 10,//アニメーションの時間設定。数字が小さくなるほど速い
+    duration: 15,//アニメーションの時間設定。数字が小さくなるほど速い
     forceRender: false,//パスが更新された場合に再レンダリングさせない
     animTimingFunction:Vivus.EASE,//動きの加速減速設定
 },
