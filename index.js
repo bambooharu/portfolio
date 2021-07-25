d8257cf5b8972b84da4fa34ba98d609b9afbc88a
bambooharu <bambooharu0802@gmail.com>
2021-07-25 23:04:16 +0900

    いろいろ変更

--- index.js
+++ index.js
@@ -13,3 +13,44 @@ function(){
 }
 );
 
+//画像の設定
+
+var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
+		if (windowwidth > 768){
+			var responsiveImage = [//PC用の画像
+				{ src: './img/cafe.jpg'},
+				{ src: './img/azisai3.JPG'},
+				{ src: './img/kissa.JPG'}
+			];
+		} else {
+			var responsiveImage = [//タブレットサイズ（768px）以下用の画像
+				{ src: './img/cafe.jpg' },
+				{ src: './img/azisai3.JPG' },
+				{ src: './img/kissa.JPG' }
+			];
+		}
+
+//Vegas全体の設定
+
+$('#slider').vegas({
+		overlay: true,//画像の上に網線やドットのオーバーレイパターン画像を指定。
+		transition: 'fade2',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
+		transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
+		delay: 5000,//スライド間の遅延をミリ秒単位で。
+		animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
+		animation: 'random',//スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
+		slides: responsiveImage,//画像設定を読む
+		//timer:false,// プログレスバーを非表示したい場合はこのコメントアウトを外してください
+	});
+
+    $(".openbtn").click(function () {//ボタンがクリックされたら
+        $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+        $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+        $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+    });
+    
+    $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+        $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+        $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+        $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+    });
